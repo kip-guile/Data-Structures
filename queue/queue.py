442a6dc1ffd60399e29f982bce51b79fe1d34d33
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-06-08 21:48:36 +0100

    queue

--- queue/queue.py
+++ queue/queue.py
@@ -15,20 +15,66 @@ Stretch: What if you could only use instances of your Stack class to implement t
 """
 
 
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.insert(0, value)
+
+#     def dequeue(self):
+#         if len(self.storage) == 0:
+#             return
+#         self.storage -= 1
+#         return self.storage.pop()
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
 class Queue:
     def __init__(self):
+        self.head = None
         self.size = 0
-        self.storage = []
 
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
+        new_node = Node(value)
         self.size += 1
-        self.storage.insert(0, value)
+        if not self.head:
+            self.head = new_node
+        else:
+            old_head = self.head
+            new_head = self.head
+            new_head.set_next(old_head)
 
     def dequeue(self):
-        if len(self.storage) == 0:
+        if not self.head:
             return
-        self.storage -= 1
-        return self.storage.pop()
+        else:
+            self.size -= 1
+            current = self.head
+            while current.get_next() is not None:
+                prev = current
+                current.get_next()
+            prev.set_next(None)
+            return prev
