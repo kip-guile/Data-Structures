204e0a40fe02bc6e4299e5d8eacecea260686590
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-06-09 13:47:17 +0100

    wip

--- stack/stack.py
+++ stack/stack.py
@@ -15,23 +15,23 @@ return elements in Last In First Out order.
 from singly_linked_list import LinkedList
 
 
-class Stack:
-    def __init__(self):
-        self.size = 0
-        self.storage = []
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
 
-    def __len__(self):
-        return self.size
+#     def __len__(self):
+#         return self.size
 
-    def push(self, value):
-        self.size += 1
-        self.storage.insert(len(self.storage) + 1, value)
+#     def push(self, value):
+#         self.size += 1
+#         self.storage.insert(len(self.storage) + 1, value)
 
-    def pop(self):
-        if len(self.storage) == 0:
-            return
-        self.size -= 1
-        return self.storage.pop()
+#     def pop(self):
+#         if len(self.storage) == 0:
+#             return
+#         self.size -= 1
+#         return self.storage.pop()
 
 
 class Stack:
